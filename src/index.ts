25b6f6d433a7f57b129469af5623a18802bf11fc
CJB <chris.berno@gmail.com>
2025-05-09 14:38:42 -0400

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import * as FlexPlugin from '@twilio/flex-plugin';
+
+import CustomerProfileIntegratorPlugin from './CustomerProfileIntegratorPlugin';
+
+FlexPlugin.loadPlugin(CustomerProfileIntegratorPlugin);
